96665d527bdfe5539c57a140f5a0290c24e11d2c
swangming <ww2741002916@163.com>
2023-06-26 21:25:16 +0800

    练习

---
+++ game 1/game 1/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+/*int main()
+{
+	char n[3][3] = { 0 };
+
+	return 0;
+}*/
+/*int main()
+{
+	int a = 10;
+	int* p = &a;
+	p = 8;
+	printf("%d", a);
+	return 0;
+}*/
+
+int main()
+{
+	printf("%d", ' ');
+	return 0;
+}
